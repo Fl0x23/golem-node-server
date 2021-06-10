a68a2c0f0b8717fd4b1d143177f871297887968b
Fl0x23 <flotzinger@web.de>
2021-06-10 14:39:02 +0200

    Add Authentication

--- app/app.py
+++ app/app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask, request
+from flask_httpauth import HTTPTokenAuth
+
 from golem import GolemStatus
 from flask_cors import CORS
+import secrets
 import hardware
 import sys
 
 app = Flask(__name__)
 
 CORS(app, resources=r'/api/*')
+
+auth = HTTPTokenAuth(scheme='Bearer')
+tokens = { 'V22PFvsrRcUf9j1ktipL_A': 'admin' }
+
+def generate_HTTPTokenAuth():
+    if sum(len(token) for token in tokens) == 0:
+        secret = secrets.token_urlsafe(16)
+        tokens[secret] = "admin"
+        return " + Random_HTTPTokenAuth: " + str(tokens)
+    else:
+        return " + HTTPTokenAuth: " + str(tokens)
 
 def hardware_stats(status):
     return {
@@ -44,6 +58,11 @@ def golem(status):
         "processedLastHour": status.processed_hour(),
     }
 
+@auth.verify_token
+def verify_token(token):
+    if token in tokens:
+        return tokens[token]
+
 @app.route('/api/status', methods=['GET'])
 def stats_all():
     golem_status = GolemStatus()
@@ -53,5 +72,12 @@ def stats_all():
         "info": golem(golem_status),
     }
 
+@app.route('/api/login', methods=['POST'])
+@auth.login_required
+def login():
+    print(request.json)
+    return "{login=true}"
+
 if __name__ == '__main__':
+    print(generate_HTTPTokenAuth())
     app.run(host='0.0.0.0')
